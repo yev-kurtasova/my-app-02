be57ffd593f666fc4a6a56de46437cf363c6577d
Yevheniia Kurtasova <eugenia.kurtasova@gmail.com>
2023-12-10 14:47:14 +0200

    create elements with .createElement

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const element1 = React.createElement('p', { 'fontSize': '40px' }, 'Розробити сторінку з заголовком, параграфом тексту та зображенням за допомогою синтаксису JSX. Повторити дану сторінку, використовуючи метод createElement.')
+  const element1 = React.createElement('h2', { 'fontFamily': 'Tahoma', 'backgroundColor': 'yellow'}, 'Task 2');
+  const element2 = React.createElement('p', { 'fontFamily': 'Tahoma', 'fontSize': '40px' }, 'Розробити сторінку з заголовком, параграфом тексту та зображенням за допомогою синтаксису JSX. Повторити дану сторінку, використовуючи метод createElement.');
+  const elementImg = React.createElement('img', { 'src': 'C:/IT/FrontEnd/React/02/hw_2/my-app-02/public/cat.jpg' }, null);
   return (
+    
     <>
       <h2 style={{ fontFamily: 'Tahoma' }}>{'Task 2'}</h2>
       <p style={{ fontFamily: 'Tahoma', color: 'darkgreen', backgroundColor: 'yellow', fontSize: '20px' }}>Розробити сторінку з заголовком, параграфом тексту та зображенням за допомогою синтаксису JSX. Повторити дану сторінку, використовуючи метод createElement.
       </p>
       <img src={logo} alt='cat'></img>
 
-
       {element1}
+      {elementImg}
+      {element2}
+
+
     </>
   );
 }
