a4efe2674ff21771ec5fd299f40e032ccc7e334b
Yevheniia Kurtasova <eugenia.kurtasova@gmail.com>
2023-12-10 14:11:40 +0200

    create elements with JSX

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const element1 = React.createElement('hq', { 'fontSize': '40px'}, 'Розробити сторінку з заголовком, параграфом тексту та зображенням за допомогою синтаксису JSX. Повторити дану сторінку, використовуючи метод createElement.')
+  const element1 = React.createElement('p', { 'fontSize': '40px' }, 'Розробити сторінку з заголовком, параграфом тексту та зображенням за допомогою синтаксису JSX. Повторити дану сторінку, використовуючи метод createElement.')
   return (
     <>
-    
-    <img src={logo} alt='cat'></img>
-    
+      <h2 style={{ fontFamily: 'Tahoma' }}>{'Task 2'}</h2>
+      <p style={{ fontFamily: 'Tahoma', color: 'darkgreen', backgroundColor: 'yellow', fontSize: '20px' }}>Розробити сторінку з заголовком, параграфом тексту та зображенням за допомогою синтаксису JSX. Повторити дану сторінку, використовуючи метод createElement.
+      </p>
+      <img src={logo} alt='cat'></img>
 
-    {element1}
+
+      {element1}
     </>
   );
 }
